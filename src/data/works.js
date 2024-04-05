9faef09d95b7a72c37fe5096127eda967ae17e33
Edgar Leonel Acevedo Cuevas <leonelstradale711@gmail.com>
2024-04-04 20:52:24 -0600

    Pre-Last Commit

---
+++ src/data/works.js
@@ -0,0 +1,35 @@
+export const works = [
+  {
+    id: "sci-benmac",
+    image: "/src/img/sci-benmac.png",
+    category: "Software",
+    name: "SCI - BENMAC",
+    description:
+      "Sistema para el Control de Impresiones dentro de la escuela normal BENMAC.",
+    url: "impresiones.benmac.edu.mx",
+    author: "Edgar Leonel Acevedo Cuevas",
+    year: "2023",
+  },
+  {
+    id: "scag-utzac",
+    image: "/src/img/scag-utzac.png",
+    category: "Software",
+    name: "SCAG - UTZAC",
+    description:
+      "Sistema para el Control de Asistencia dentro del Gimnasio de la UTZAC.",
+    url: "#",
+    author: "Edgar Leonel Acevedo Cuevas",
+    year: "2023",
+  },
+  {
+    id: "e-commerce",
+    image: "/src/img/e-commerce.png",
+    category: "Software",
+    name: "E-Commerce",
+    description:
+      "Tienda Online para productos en general profesional personalizable.",
+    url: "#",
+    author: "Edgar Leonel Acevedo Cuevas",
+    year: "2024",
+  },
+];
